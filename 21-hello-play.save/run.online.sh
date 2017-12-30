596acc53ae68f6e4b06a59474f6319ed93df6865
Philippe Charrière <ph.charriere@gmail.com>
2017-12-30 08:55:41 +0100

    remove old directory

--- 21-hello-play.save/run.online.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Dsbt.ivy.home="../_ivy2" -Dsbt.global.base="../_sbt" -Dsbt.boot.directory="../_sbt/boot" -Xmx512M -jar -Dfile.encoding=UTF8 -Xss1M -XX:+CMSClassUnloadingEnabled ../sbt/bin/sbt-launch.jar run
