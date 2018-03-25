36f4c88b9f2cb8303405a8d7bdff68652987b333
k33g <pcharriere@gitlab.com>
2018-03-25 14:39:31 +0200

    linux 32 gardening

--- osx.setupenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# . osx.setupenv.sh
-
-export JAVA_HOME=$(pwd)/jdk1.8.0_151-osx/java-osx
-export PATH=$JAVA_HOME/bin:$PATH
-echo "Java Home: $JAVA_HOME"
-$JAVA_HOME/bin/java -version
--- tux.setupenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# . tux.setupenv.sh
-
-export JAVA_HOME=$(pwd)/jdk1.8.0_151-linux-x64/java-linux-x64
-export PATH=$JAVA_HOME/bin:$PATH
-echo "Java Home: $JAVA_HOME"
-$JAVA_HOME/bin/java -version
