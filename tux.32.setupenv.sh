abae3ef3257eecfcb5d2a44170c75afcc530625c
k33g <pcharriere@gitlab.com>
2018-03-25 14:46:08 +0200

    win 32 gardening

--- tux.32.setupenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# . tux.setupenv.sh
-
-export JAVA_HOME=$(pwd)/jdk1.8.0_161-linux/java-linux
-export PATH=$JAVA_HOME/bin:$PATH
-echo "Java Home: $JAVA_HOME"
-$JAVA_HOME/bin/java -version
--- win.setupenv.cmd
+++
@@ -1,5 +0,0 @@
-echo off
-set JAVA_HOME=%cd%\jdk1.8.0_151-windows-x64\java-windows
-set PATH=%PATH%;%JAVA_HOME%\bin
-%JAVA_HOME%\bin\java -version
-echo on
